61d4585e910162d3ea7360c81387fcbfe9152d71
Pavel3737 <118966084+Pavel3737@users.noreply.github.com>
2022-12-12 14:21:53 +0300

    Add files via upload

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Say("Hello world!");
+            Console.WriteLine(Sum(2, 4));
+        }
+
+        static void Say(string str)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(str);
+            }
+        }
+
+        static int Sum(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}
